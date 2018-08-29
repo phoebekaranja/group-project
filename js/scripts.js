c2a2257d9ad5d76a4f01db1c75a815dc9e991494
phoebekaranja <phoebemurish@gmail.com>
2018-08-29 16:28:34 +0300

    Add changes to group-html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document) .ready (function() {
+$("blanks").submit (function(event) {
+var person 1 input=$("input#person 1").val();
+var name=$("input#name").val();
+var email=$("input#email").val();
+var qualification=$("input#qualification").val();
+$("#person1").text(person name 1);
+$("#qualification").text(qualification);
+$("email").alphanumeric(emailaddress);
+event.prevent(Default();
+var person = function(name, email) {
+ return name + email;
+};
+var person = parseInt(prompt("Enter a number:"));
+var qualification= parseInt(prompt("Enter another number:"));
