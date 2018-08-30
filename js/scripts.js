84fe2e0fdcf753d4d912a401f4f8fbab6ab9d609
phoebekaranja <phoebemurish@gmail.com>
2018-08-30 10:36:12 +0300

    Added an image

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-$(document) .ready (function() {
-$("blanks").submit (function(event) {
-var person 1 input=$("input#person 1").val();
-var name=$("input#name").val();
-var email=$("input#email").val();
-var qualification=$("input#qualification").val();
-$("#person1").text(person name 1);
-$("#qualification").text(qualification);
-$("email").alphanumeric(emailaddress);
-event.prevent(Default();
-var person = function(name, email) {
- return name + email;
-};
-var person = parseInt(prompt("Enter a number:"));
-var qualification= parseInt(prompt("Enter another number:"));
+// Validating Empty Field
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
